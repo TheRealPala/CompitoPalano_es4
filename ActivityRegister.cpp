ca817179905ad10bf489aa1dd1f66b114db05820
Alessandro Palano <apalano23@gmail.com>
2023-06-27 17:01:30 +0200

    FEAT: Added ActivityRegister class

---
+++ ActivityRegister.cpp
@@ -0,0 +1,27 @@
+//
+// Created by ale on 27/06/23.
+//
+
+#include <algorithm>
+#include <iostream>
+#include "ActivityRegister.h"
+
+void ActivityRegister::addActivity(const Activity &activity) {
+    activities.push_back(activity);
+    oreTotali += activity.getOre();
+    durataTotale += activity.getDurata();
+    calorieTotali += activity.getKcal();
+}
+
+void ActivityRegister::removeActivity(const Activity& activity) {
+    activities.erase(std::remove(activities.begin(), activities.end(), activity), activities.end());
+    oreTotali -= activity.getOre();
+    durataTotale -= activity.getDurata();
+    calorieTotali -= activity.getKcal();
+}
+
+void ActivityRegister::getStatus() {
+    std::cout << "Ore totali: " << oreTotali << std::endl;
+    std::cout << "Durata totale: " << durataTotale << std::endl;
+    std::cout << "Calorie totali: " << calorieTotali << std::endl;
+}
---
+++ ActivityRegister.h
@@ -0,0 +1,30 @@
+//
+// Created by ale on 27/06/23.
+//
+
+#ifndef PALANO_ES4_ACTIVITYREGISTER_H
+#define PALANO_ES4_ACTIVITYREGISTER_H
+
+
+#include <list>
+#include "Activity.h"
+
+class ActivityRegister {
+private:
+    float oreTotali;
+    float durataTotale;
+    float calorieTotali;
+    std::vector<Activity> activities;
+public:
+    ActivityRegister(){
+        oreTotali = 0;
+        durataTotale = 0;
+        calorieTotali = 0;
+    }
+    void addActivity(const Activity& activity);
+    void removeActivity(const Activity& activity);
+    void getStatus();
+};
+
+
+#endif //PALANO_ES4_ACTIVITYREGISTER_H
