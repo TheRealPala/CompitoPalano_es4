2254281d4301d9742c3261d99dc8c06b49d68e32
Alessandro Palano <apalano23@gmail.com>
2023-06-27 17:10:09 +0200

    FEAT: Added main and test code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include "ActivityRegister.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ActivityRegister ar;
+    Activity a1 = Activity(2, 1.5, 350, "Activity 1", 10);
+    ar.addActivity(a1);
+    ar.getStatus();
+    std::cout << std::endl;
+    Activity a2 = Activity(3, 2, 480, "Activity 2", 20);
+    ar.addActivity(a2);
+    ar.getStatus();
+    std::cout << std::endl;
+    ar.removeActivity(a1);
+    ar.getStatus();
     return 0;
 }
